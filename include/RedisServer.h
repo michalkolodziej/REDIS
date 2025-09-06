429e575d7cd84ebad5a07888e42900252690f4cf
Michal <michalkolodziej@tuta.io>
2025-09-06 22:11:46 +0200

    Basic functionality of server added

--- include/RedisServer.h
+++ include/RedisServer.h
@@ -4,13 +4,24 @@
 
 #ifndef REDIS_REDISSERVER_H
 #define REDIS_REDISSERVER_H
+#include <atomic>
 #include <iostream>
+#include <string>
+#include <atomic>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
 class RedisServer{
 public:
     RedisServer(int port);
+    void run();
+    void stop();
 private:
     int port;
+    int server_socket;
+    std::atomic<bool> running;
+
 };
 
 #endif //REDIS_REDISSERVER_H
--- src/RedisServer.cpp
+++ src/RedisServer.cpp
@@ -3,8 +3,40 @@
 //
 
 #include "../include/RedisServer.h"
-RedisServer::RedisServer(int port) {
-    this->port = port;
 
-    std::cout<<"RedisServer::RedisServer()"<<std::endl;
+static RedisServer* globalServer = nullptr;
+
+RedisServer::RedisServer(int port) :port(port), server_socket(-1),running(true){
+    globalServer = this;
+}
+
+void RedisServer::stop() {
+    running = false;
+    if (server_socket != -1) {
+        close(server_socket);
+    }
+    std::cout << "RedisServer::stop)" << std::endl;
+}
+void RedisServer::run() {
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        std::cerr << "Error creating server socket" << std::endl;
+        return;
+    }
+    int opt = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(port);
+    server_address.sin_port.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        std::cerr << "Error binding server socket to port" << std::endl;
+        return;
+    }
+    if (listen(server_socket, 10) < 0) {
+        std::cerr << "Error listening on port" << std::endl;
+        return;
+    }
+    std::cout << "RedisServer::run() port: " << port << std::endl;
 }
